a82f430c11f267f0c0d53ce92d82e831261a249e
Thiago Souto <soutosss@gmail.com>
2021-04-24 15:25:40 -0300

    feat(charts): create interactions and state for charts activity

---
+++ features/charts/src/main/kotlin/dev/thiagosouto/blockchain/features/charts/ChartsInteractions.kt
@@ -0,0 +1,5 @@
+package dev.thiagosouto.blockchain.features.charts
+
+sealed class ChartsInteractions {
+    object OpenedScreen : ChartsInteractions()
+}
---
+++ features/charts/src/main/kotlin/dev/thiagosouto/blockchain/features/charts/ChartsScreenState.kt
@@ -0,0 +1,15 @@
+package dev.thiagosouto.blockchain.features.charts
+
+import androidx.annotation.StringRes
+import dev.thiagosouto.blockchain.domain.Chart
+
+sealed class ChartsScreenState {
+    object Idle : ChartsScreenState()
+    object Loading : ChartsScreenState()
+    data class Success(val chart: Chart) : ChartsScreenState()
+    data class Failed(
+        @StringRes val titleResId: Int,
+        @StringRes val descriptionResId: Int,
+        val retryAction: () -> Unit
+    ) : ChartsScreenState()
+}
