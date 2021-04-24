708315d8219ea697521419a400510c9d0133e5a0
Thiago Souto <soutosss@gmail.com>
2021-04-24 19:20:13 -0300

    test(charts): provide way to do manual testing retry

--- features/charts/src/androidTest/kotlin/dev/thiagosouto/blockchain/features/charts/ChartsActivityTest.kt
+++ features/charts/src/androidTest/kotlin/dev/thiagosouto/blockchain/features/charts/ChartsActivityTest.kt
@@ -6,6 +6,7 @@ import dev.thiagosouto.blockchain.domain.Axis
 import dev.thiagosouto.blockchain.domain.Chart
 import dev.thiagosouto.blockchain.domain.ChartParameters
 import dev.thiagosouto.blockchain.domain.ChartRepository
+import dev.thiagosouto.blockchain.domain.exception.InternetNotAvailableException
 import dev.thiagosouto.blockchain.features.charts.di.chartsModules
 import dev.thiagosouto.blockchain.features.charts.rules.KoinTestRule
 import kotlinx.coroutines.delay
@@ -14,6 +15,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.dsl.module
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 @Ignore("This class is just to help me do manual testing when coding")
@@ -26,14 +28,19 @@ class ChartsActivityTest {
     @Test
     fun shouldInitScreenCorrectly() {
         ActivityScenario.launch(ChartsActivity::class.java)
-        Thread.sleep(15_000)
+        Thread.sleep(20_000)
     }
 }
 
 class FakeRepository : ChartRepository {
+    private var isFirst = true
     override suspend fun getChart(params: ChartParameters): Chart {
         delay(2_000)
-       return  Chart(
+        if (isFirst) {
+            isFirst = false
+            throw InternetNotAvailableException(IOException())
+        }
+        return Chart(
             axis = listOf(
                 Axis(1000, 1.0f),
                 Axis(2000, 2.0f),
