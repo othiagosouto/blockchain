e138aa11833dc82a6fbe1bb0611fdbe06fb4806e
Thiago Souto <soutosss@gmail.com>
2021-04-24 16:19:29 -0300

    feat(charts): create di module

---
+++ features/charts/src/main/kotlin/dev/thiagosouto/blockchain/features/charts/di/modules.kt
@@ -0,0 +1,9 @@
+package dev.thiagosouto.blockchain.features.charts.di
+
+import dev.thiagosouto.blockchain.features.charts.ChartsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val chartsModules = module {
+    viewModel { ChartsViewModel(get()) }
+}
