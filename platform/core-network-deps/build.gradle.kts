a9e76e7e21f535ef52efd89ef9b275200d187f1f
Thiago Souto <soutosss@gmail.com>
2021-04-24 00:00:32 -0300

    build: add okhttp to core-networks-deps

--- platform/core-network-deps/build.gradle.kts
+++ platform/core-network-deps/build.gradle.kts
@@ -7,5 +7,6 @@ dependencies {
         api("com.squareup.retrofit2:retrofit:2.9.0")
         api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
         api("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+        api("com.squareup.okhttp3:okhttp:4.9.1")
     }
 }
