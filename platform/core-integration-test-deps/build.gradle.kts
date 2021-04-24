5aea0f2da3cd7cf08451957fa8accfe0db3be83c
Thiago Souto <soutosss@gmail.com>
2021-04-23 22:48:26 -0300

    build: create :platform:core-integration-test-deps to group integration test dependencies

---
+++ platform/core-integration-test-deps/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    `java-platform`
+}
+
+dependencies {
+    val espressoVersion = "3.3.0"
+    val androidXTestVersion = "1.3.0"
+    val androidxTestExtJunitVersion = "1.1.2"
+    val robolectricVersion = "4.5.1"
+
+    constraints {
+        api("androidx.test:core:$androidXTestVersion")
+        api("androidx.test.espresso:espresso-contrib:$espressoVersion")
+        api("androidx.test.espresso:espresso-core:$espressoVersion")
+        api("androidx.test.espresso:espresso-intents:$espressoVersion")
+        api("androidx.test.ext:junit:$androidxTestExtJunitVersion")
+        api("androidx.test:runner:$androidXTestVersion")
+        api("org.robolectric:robolectric:$robolectricVersion")
+    }
+}
