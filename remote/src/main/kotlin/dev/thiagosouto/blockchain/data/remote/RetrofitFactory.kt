4742876bd4bbe2fc50b0a50a3295dfe8829c77b6
Thiago Souto <soutosss@gmail.com>
2021-04-24 00:37:47 -0300

    feat(remote): create retrofit factory

---
+++ remote/src/main/kotlin/dev/thiagosouto/blockchain/data/remote/RetrofitFactory.kt
@@ -0,0 +1,18 @@
+package dev.thiagosouto.blockchain.data.remote
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+class RetrofitFactory {
+
+    @kotlinx.serialization.ExperimentalSerializationApi
+    fun create(serverUrl: String): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(serverUrl)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+}
