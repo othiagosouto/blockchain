3b76a67437e4a34a3cdedff43d93f22e16e57645
Thiago Souto <soutosss@gmail.com>
2021-04-23 22:37:37 -0300

    build: create :platform:core-network-deps to group network deps

---
+++ platform/core-network-deps/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    `java-platform`
+}
+
+dependencies {
+    constraints {
+        api("com.squareup.retrofit2:retrofit:2.9.0")
+        api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+        api("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ rootProject.name = "Blockchain"
 include(":app")
 include(":platform:core-android-deps")
 include(":platform:core-kotlin-deps")
-
+include(":platform:core-network-deps")
